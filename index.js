e040e685f43b5d0005a73f4bd860dbcfe851cb66
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2025-11-10 09:27:39 +0700

    done

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const express = require('express'); // Import the Express module
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+import express from 'express'; // Import the Express module
+import { Redis } from '@upstash/redis'
+
+const redis = Redis.fromEnv();
+
+// await redis.set('foo', 'bar');
+// const data = await redis.get('foo');
+
 const app = express(); // Create an Express application instance
 const PORT = process.env.PORT || 3000; // Define the port, using environment variable if available
 
@@ -8,7 +19,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getdata', (req, res) => {
+    redis.set('data', req.query.data);
     res.send(req.query.data);
+});
+
+app.get('/data', (req, res) => {
+    redis.get('data')
+    .then(r => res.send(r));
+});
+
+app.get('/status', (req, res) => {
+    redis.get('data')
+    .then(r => {
+      let output = [0, 0]
+      r.split(",");
+      if (r[0] > 35 || r[1] < 50 || r[2] > 6000) output[0] = 1
+      if (r[3] > 35 || r[4] < 50 || r[5] > 6000) output[1] = 1
+      res.send(output.toString())
+    });
 });
 
 // Start the server and listen for incoming requests on the specified port
